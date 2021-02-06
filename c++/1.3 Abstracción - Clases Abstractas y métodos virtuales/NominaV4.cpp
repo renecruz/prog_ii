e30113743248c38641ba0fd863b02deb957cc2f1
René Cruz Flores <renecruzflores1@gmail.com>
2021-02-06 15:09:36 -0600

    Update: C++ 1.3 Projects

---
+++ c++/1.3 Abstracción - Clases Abstractas y métodos virtuales/NominaV4.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+
+using namespace std;
+
+class Vendedor {
+  private:
+    string nombre;
+    float sueldoBase;
+
+  public:
+    Vendedor(string nombre, float sueldoBase) {
+      this->nombre = nombre;
+      this->sueldoBase = sueldoBase;
+    }
+
+    float getSueldoBase() {
+      return this->sueldoBase;
+    }
+
+    string getNombre() {
+      return nombre;
+    }
+
+    virtual float calculaSueldo() = 0;
+};
+
+class VendedorBase : public Vendedor {
+  private:
+    string depto;
+
+  public:
+    VendedorBase(string nombre, float sueldoBase, string depto) : Vendedor(nombre, sueldoBase) {
+      this->depto = depto;
+    }
+
+    float calculaSueldo() {
+      return Vendedor::getSueldoBase();
+    }
+};
+
+class VendedorPorComision : public Vendedor {
+  private:
+    int ventasRealizadas;
+    const float COMISION_POR_VENTA = 50.0f;
+
+  public:
+    VendedorPorComision(string nombre, float sueldoBase, int ventasRealizadas) : Vendedor(nombre, sueldoBase){
+      this->ventasRealizadas = ventasRealizadas;
+    }
+
+    float calculaSueldo() {
+      return Vendedor::getSueldoBase() + (ventasRealizadas * COMISION_POR_VENTA);
+    }
+};
+
+class VendedorEventual : public Vendedor {
+  private:
+    int diasTrabajados;
+    const float SUELDO_POR_DIA = 100.0f;
+
+  public:
+    VendedorEventual(string nombre, float sueldoBase, int diasTrabajados) : Vendedor(nombre, sueldoBase) {
+      this->diasTrabajados = diasTrabajados;
+    }
+
+    float calculaSueldo() {
+      return Vendedor::getSueldoBase() + (diasTrabajados * SUELDO_POR_DIA);
+    }
+};
+
+int main(int argc, char const *argv[])
+{ 
+  float nomina = 0;
+
+  Vendedor *vendedores[3];
+  vendedores[0] = new VendedorBase("Rosa Ma. López", 3500.0f, "Electrónicos");
+  vendedores[1] = new VendedorPorComision("José Rodríguez", 3250.0f, 10);
+  vendedores[2] = new VendedorEventual("Julia Vázquez ", 2000.0f, 4);
+  
+  int len = sizeof(vendedores)/sizeof(vendedores[0]);
+
+  for (int i=0; i<len; i++ ) {
+    nomina = nomina + vendedores[i]->calculaSueldo();
+    cout << vendedores[i]->getNombre() << ", Sueldo: $" << vendedores[i]->calculaSueldo() << endl;
+  }
+
+  cout << "Total de vendedores: " << len << ", Nómina a pagar: $" << nomina << endl;
+
+  return 0;
+}
+// Compilar como: g++ -std=c++11 NominaV4.cpp -o NominaV4
+// para evitar los Warnings por el uso de constantes globales...
