47c4a769f653d91097bfb71efe7aacb6779098fc
René Cruz Flores <renecruzflores1@gmail.com>
2021-02-13 16:13:34 -0600

    Update ArrayListTest.java

--- java/1.4 Iteradores y Colecciones de Objetos/1.4.3 Colecciones/Ejemplo03/com/ditamex/main/ArrayListTest.java
+++ java/1.4 Iteradores y Colecciones de Objetos/1.4.3 Colecciones/Ejemplo03/com/ditamex/main/ArrayListTest.java
@@ -1,45 +1,34 @@
-package com.ditamex.main;
-
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class ArrayListTest
-{
+public class ArrayListTest {
     /*
      * Revisar la documentación de la clase Collections
      * https://docs.oracle.com/javase/8/docs/api/java/util/Collections.html
      */
-    
+
     public static void main(String args[]) {
-        ArrayList colores = new ArrayList();
-        
+        ArrayList<String> colores = new ArrayList<String>();
+
         colores.add("Rojo");
         colores.add("Blanco");
         colores.add("Amarillo");
         colores.add("Negro");
-        colores.add("Azul"); 
-        
+        colores.add("Azul");
+
         System.out.println("-Desordenado-");
-        for (int x = 0; x < colores.size(); x++) {
-            System.out.println( "Color: " + colores.get(x) );
-        }
-        
+        System.out.println("Colores: " + colores);
+
         Collections.sort(colores);
         System.out.println("-Ordenado-");
-        for (int x = 0; x < colores.size(); x++) {
-            System.out.println( "Color: " + colores.get(x) );
-        }
+        System.out.println("Colores: " + colores);
 
         Collections.reverse(colores);
         System.out.println("-Reversa-");
-        for (int x = 0; x < colores.size(); x++) {
-            System.out.println( "Color: " + colores.get(x) );
-        }
-        
+        System.out.println("Colores: " + colores);
+
         Collections.shuffle(colores);
         System.out.println("-Aleatorio-");
-        for (int x = 0; x < colores.size(); x++) {
-            System.out.println( "Color: " + colores.get(x) );
-        }
+        System.out.println("Colores: " + colores);
     }
 }
